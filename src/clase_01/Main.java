f6f962660985f280b4d41d867bc025ed44d2e092
Sergio <sopekof@gmail.com>
2026-09-13 17:17:40 -0300

    curso-java

---
+++ src/clase_01/Main.java
@@ -0,0 +1,12 @@
+package clase_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\n ----- CLASE 01 ----- \n");
+
+        System.out.println("Hola Mundo desde Java!!!!");
+
+    }
+
+}
---
+++ src/clase_02/Main.java
@@ -0,0 +1,14 @@
+package clase_02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\n ----- CLASE 02 ----- \n");
+
+        System.out.println("Hola desde clase_02!!!!");
+
+        // En Java las Clases siempre empiezan con mayúscula y los métodos con minúscula.
+
+    }
+
+}
